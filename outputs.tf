4e0f12db5e8fc179c64a1be38e1de02feff17969
Matheus Fuzati <fuzatimatheus@gmail.com>
2026-03-11 17:03:04 -0300

    outputs.tf | 2 +-

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "dataform_console_url" {
-  value = "https://console.cloud.google.com/bigquery/dataform/locations/${var.region}/repositories/${google_dataform_repository.martech_repo.name}/details?project=${var.project_id}"
+  value       = "https://console.cloud.google.com/bigquery/dataform/locations/${var.region}/repositories/${google_dataform_repository.martech_repo.name}/details?project=${var.project_id}"
   description = "Link direto para o repositório Dataform no GCP"
-}git remote add origin https://github.com/Matheus-Fuzati-de-Carvalho/beta-toolkit-martech-infra.git
+}
